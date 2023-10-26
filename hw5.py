c7d95000dff3f9d8d86db2b101acb46ddb0f44e4
apmorr <144373529+apmorr@users.noreply.github.com>
2023-10-26 19:38:17 -0400

    Update hw5.py

--- hw5.py
+++ hw5.py
@@ -1,17 +1,23 @@
-def valid_moves(k_idx):
-    row, col = k_idx
-    possible_moves = {(row - 1, col - 2), (row - 2, col - 1), (row - 2, col + 1), (row - 1, col + 2),
-                      (row + 1, col - 2), (row + 2, col - 1), (row + 2, col + 1), (row + 1, col + 2)}
-    return set(filter(lambda x: 0 <= x[0] <= 7 and 0 <= x[1] <= 7, possible_moves))
-
-
 def solveable(p_idxs, k_idx):
-    if not p_idxs:  # base case, no pawns left to capture
+    """Returns True (false) if all pawn locations can be capture by sequential knight moves"""
+
+    if not p_idxs:  # base case - puzzle is solved
         return True
-    moves = valid_moves(k_idx)
+    moves = valid_moves(k_idx)  # Finds all valid moves
     for move in moves:
-        if move in p_idxs:  # capture pawn and move knight
+        if move in p_idxs:  # trys all valid moves
             new_pawns = p_idxs - {move}
             if solveable(new_pawns, move):
                 return True
-    return False
+    return False  # if all failed returns false
+
+
+def valid_moves(k_idx):
+    """Returns set of all valid moves from k_idx, assuming an 8x8 chess board"""
+    row, col = k_idx
+    possible_moves = {(row - 1, col - 2), (row - 2, col - 1), (row - 2, col + 1), (row - 1, col + 2),
+                      (row + 1, col - 2), (row + 2, col - 1), (row + 2, col + 1), (row + 1, col + 2)}
+    # creates a list of all possible moves at the given point
+
+    return set(filter(lambda x: 0 <= x[0] <= 7 and 0 <= x[1] <= 7, possible_moves))
+    # compares each item in the list, only returns the moves inside the 8x8 chessboard
