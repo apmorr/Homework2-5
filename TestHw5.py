4461d789314dbd0f39e55322d33010706cc01862
apmorr <144373529+apmorr@users.noreply.github.com>
2023-10-26 19:37:33 -0400

    Update TestHw5.py

--- TestHw5.py
+++ TestHw5.py
@@ -2,17 +2,33 @@ from hw5 import solveable, valid_moves
 import unittest
 
 
-class TestChessboard(unittest.TestCase):
+class TestValidMoves(unittest.TestCase):
 
-    def test_valid_moves(self):
-        self.assertCountEqual(valid_moves((0, 0)), {(1, 2), (2, 1)})
-        self.assertCountEqual(valid_moves((2, 2)), {(0, 1), (0, 3), (1, 0), (1, 4), (3, 0), (3, 4), (4, 1), (4, 3)})
-        self.assertCountEqual(valid_moves((7, 7)), {(5, 6), (6, 5)})
+    def testValidMoves(self):
+        self.assertEqual(valid_moves((0, 0)), {(1, 2), (2, 1)})
+        self.assertEqual(valid_moves((2, 2)), {(0, 1), (0, 3), (1, 0), (1, 4), (3, 0), (3, 4), (4, 1), (4, 3)})
+        self.assertEqual(valid_moves((7, 7)), {(5, 6), (6, 5)})
 
-    def test_solveable(self):
-        self.assertTrue(solveable({(1, 5), (2, 1), (2, 5), (4, 2), (5, 5), (6, 3)}, (3, 3)))
+
+class TestSolveable(unittest.TestCase):
+
+    def testUnsolveable(self):
+        """Test a few unsolveable puzzles"""
         self.assertFalse(solveable({(1, 3), (2, 1), (2, 5), (4, 2), (5, 5)}, (3, 3)))
         self.assertFalse(solveable({(1, 3), (2, 1), (2, 5), (4, 2), (5, 5), (6, 3)}, (0, 0)))
-        self.assertTrue(solveable(set(), (3, 3)))
 
+    def testSolveableSimple(self):
+        """Test a simple solveable puzzle"""
+        self.assertTrue(solveable({(1, 3), (2, 1), (4, 2)}, (2, 3)))
+        self.assertTrue(solveable({(1, 4), (2, 2), (3, 5), (4, 1), (6, 2)}, (5, 4)))
+
+    def testSolveableHard(self):
+        """Test a few more complex solveable puzzles - try to break your recursive algorithm to help you catch any
+        mistakes"""
+        self.assertTrue(solveable(
+            {(0, 6), (1, 3), (2, 0), (2, 1), (2, 5), (2, 7), (3, 5), (4, 1), (4, 2), (5, 4), (6, 2), (6, 6), (7, 4)},
+            (2, 3)))
+
+
+if __name__ == '__main__':
     unittest.main()
