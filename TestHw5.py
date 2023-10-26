c7e19d779e35cfcf065ce3291e70e5d32e06384a
apmorr <144373529+apmorr@users.noreply.github.com>
2023-10-26 18:16:57 -0400

    Update TestHw5.py

--- TestHw5.py
+++ TestHw5.py
@@ -1,4 +1,5 @@
 import unittest
+from hw5 import Chessboard
 
 class TestChessboard(unittest.TestCase):
     def test_solvable_board(self):
@@ -16,37 +17,3 @@ class TestChessboard(unittest.TestCase):
 if __name__ == '__main__':
     unittest.main()
 
- hw5.py
- Download
-class Chessboard:
-    def __init__(self, size, k_idx, p_idxs):
-        self.size = size
-        self.k_idx = k_idx
-        self.p_idxs = p_idxs
-        self.visited = set()
-
-    def valid_moves(self, k_idx):
-        moves = []
-        x, y = k_idx
-        for dx, dy in [(1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1), (-2, 1), (-1, 2)]:
-            new_x, new_y = x + dx, y + dy
-            if 0 <= new_x < self.size and 0 <= new_y < self.size:
-                moves.append((new_x, new_y))
-        return moves
-
-    def solveable(self):
-        if not self.p_idxs:
-            return True  # No pawns left, the board is solvable
-
-        if self.k_idx in self.visited:
-            return False  # Knight visited this cell before
-
-        self.visited.add(self.k_idx)
-
-        for move in self.valid_moves(self.k_idx):
-            if move in self.p_idxs:
-                new_board = Chessboard(self.size, move, self.p_idxs - {move})
-                if new_board.solveable():
-                    return True
-
-        return False
